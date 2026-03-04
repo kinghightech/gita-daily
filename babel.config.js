f611ba500e21b484464cce08d1e8da881ff1e24d
Aahish <Shibe.aahish@gmail.com>
2026-03-03 19:37:59 -0500

    added accordian to the app

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo", "nativewind/babel"],
+    presets: ["babel-preset-expo"],
     plugins: [
+      "expo-router/babel",
+      "nativewind/babel",
       "react-native-reanimated/plugin",
     ],
   };
